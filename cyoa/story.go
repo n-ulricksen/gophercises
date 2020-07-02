9da57b584681747b7815421dedd4f830f73ad2ba
Nicholas Ulricksen <n.ulricksen@gmail.com>
2020-07-01 21:12:09 -0700

    cyoa: add styling to template

--- cyoa/story.go
+++ cyoa/story.go
@@ -23,17 +23,59 @@ var defaultHandlerTemplate = `
 <head>
 	<meta charset="UTF-8">
 	<title></title>
+	<style>
+		body {
+			font-family: helvetica, arial;
+		}
+		h1 {
+        	text-align:center;
+        	position:relative;
+      	}
+      	.page {
+        	width: 80%;
+        	max-width: 500px;
+        	margin: auto;
+        	margin-top: 40px;
+        	margin-bottom: 40px;
+        	padding: 80px;
+        	background: #FCF6FC;
+        	border: 1px solid #eee;
+        	box-shadow: 0 10px 6px -6px #797;
+      	}
+		ul {
+        	border-top: 1px dotted #ccc;
+        	padding: 10px 0 0 0;
+        	-webkit-padding-start: 0;
+      	}
+      	li {
+        	padding-top: 10px;
+      	}
+      	a,
+      	a:visited {
+        	text-decoration: underline;
+        	color: #555;
+      	}
+		a:active,
+      	a:hover {
+        	color: #222;
+      	}
+      	p {
+        	text-indent: 1em;
+      	}
+	</style>
 </head>
 <body>
-    <h1>{{.Title}}</h1>
-    {{range .Paragraphs}}
-        <p>{{.}}</p>
-    {{end}}
-    <ul>
-		{{range .Options}}
-        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-		{{end}}
-    </ul>
+	<section class="page">
+	    <h1>{{.Title}}</h1>
+	    {{range .Paragraphs}}
+	        <p>{{.}}</p>
+	    {{end}}
+	    <ul>
+			{{range .Options}}
+	        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+			{{end}}
+	    </ul>
+	</section>
 </body>
 </html>
 `
