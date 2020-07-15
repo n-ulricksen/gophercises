fb34ca2ec8cade048c851b6432effa43cfdd4bb9
Nicholas Ulricksen <n.ulricksen@gmail.com>
2020-07-15 02:23:48 -0700

    successfully connecting to local psql

--- phone-number-normalizer/go.mod
+++ phone-number-normalizer/go.mod
@@ -1,3 +1,5 @@
 module github.com/ulricksennick/gohpercises/phone-number-normalizer
 
 go 1.14
+
+require github.com/lib/pq v1.7.0
--- phone-number-normalizer/main.go
+++ phone-number-normalizer/main.go
@@ -2,11 +2,40 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
 	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "phone_number"
 )
 
 func main() {
 	fmt.Println("vim-go")
+
+	// Connect to Postgres
+	psqlInfo := fmt.Sprintf("host=%v port=%d user=%v password=%v", host, port,
+		user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	must(err)
+
+	// Reset database to demonstrate normalize function
+	must(resetDB(db, dbname))
+	db.Close()
+
+	// Append database name, connect to database
+	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	must(err)
+	must(db.Ping())
+	defer db.Close()
 }
 
 func normalize(phone string) string {
@@ -17,4 +46,27 @@ func normalize(phone string) string {
 		}
 	}
 	return buf.String()
+}
+
+func resetDB(db *sql.DB, name string) error {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
+	if err != nil {
+		return err
+	}
+	return createDB(db, name)
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	createDB(db, name)
+	return nil
+}
+
+func must(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
